9c20e59e7be0addf8c6f81217fa5b1aaa4227311
rahul bisht <rahul.b4749@gmail.com>
2020-08-13 19:52:10 +0530

    Updates

---
+++ precache-manifest.d4f45686dbefff59f3ede922d9f8a723.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a15a104de885412d26d751cc3d668ff",
+    "url": "/youtube-clone/index.html"
+  },
+  {
+    "revision": "404cf51318247ba44534",
+    "url": "/youtube-clone/static/css/main.0328189e.chunk.css"
+  },
+  {
+    "revision": "388a689f8d19da999fb3",
+    "url": "/youtube-clone/static/js/2.d80cb4ac.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/youtube-clone/static/js/2.d80cb4ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "404cf51318247ba44534",
+    "url": "/youtube-clone/static/js/main.e4772c2e.chunk.js"
+  },
+  {
+    "revision": "8a09cd451cb85b60110f",
+    "url": "/youtube-clone/static/js/runtime-main.a270c926.js"
+  }
+]);
